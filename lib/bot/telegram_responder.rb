02f3b3b9fd38ebd6fe0aca842b9e0cc63fca3a87
denys <d.taradada@ticketstravelnetwork.com>
2021-11-28 19:05:16 +0200

    set basic structure for btns

--- lib/bot/telegram_responder.rb
+++ lib/bot/telegram_responder.rb
@@ -1,25 +1,65 @@
 require_relative "buttons"
-require_relative "../weather/weather_decorator"
+require_relative 'buttons_composer'
 
 module Bot
   class TelegramResponder
     include Buttons
-    attr_reader :bot, :message, :weather_decorator
+    attr_reader :bot, :message, :user
 
     def initialize(args)
       @bot = args[:bot]
       @message = args[:message]
-      @weather_decorator = args[:weather_data]
+      @user = args[:user]
       # @user = User.find_or_create_by(uid: message.from.id) # TODO: add connection
     end
 
     def respond
       case message
       when Telegram::Bot::Types::CallbackQuery
-        send_message(weather_decorator.set_current_weather) if message.data.include?("current_weather")
-        send_message(weather_decorator.weather_for_tomorrow) if message.data.include?("weather_for_tomorrow")
+        p message.data
+        # send_message(weather_decorator.set_current_weather) if message.data.include?("current_weather")
+        # send_message(weather_decorator.weather_for_tomorrow) if message.data.include?("weather_for_tomorrow")
+        markup = Bot::ButtonsComposer.new(
+          user: user,
+          data: message.data,
+          structure: {
+            button_1: %i[bla1],
+            button_2: {
+              bla3: %i[bla3 bla4],
+              bla4: %i[bla4 bla5],
+            }
+          }
+        ).generate_markup
+
+        # keyboard_markup = markup[:buttons]
+        # callback = markup[:callback]
+
+
+        bot.api.send_message(
+          chat_id:      message.from.id,
+          text:         markup[:callback],
+          reply_markup: markup[:buttons]
+        )
       when Telegram::Bot::Types::Message
-        send_message("Make a choice")
+        markup = Bot::ButtonsComposer.new(
+          user: user,
+          data: message.data,
+          structure: {
+            button_1: %i[bla1],
+            button_2: {
+              bla3: %i[bla3 bla4],
+              bla4: %i[bla4 bla5],
+            }
+          }
+        ).generate_markup
+
+        bot.api.send_message(
+          chat_id:      message.from.id,
+          text:         markup[:callback],
+          reply_markup: markup[:buttons]
+        )
+
+        # send_message("Make a choice")
       else
         puts "Something error"
       end
